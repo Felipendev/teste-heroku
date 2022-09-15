03519a2add92b141e88d60364a9c97714203a4a5
Danilo1dev <danilofoxlog@gmail.com>
2022-09-14 23:46:32 -0300

    implementa controller

--- src/main/java/br/com/elo7/contraladorsondas/planeta/application/api/PlanetaAPI.java
+++ src/main/java/br/com/elo7/contraladorsondas/planeta/application/api/PlanetaAPI.java
@@ -1,9 +1,11 @@
 package br.com.elo7.contraladorsondas.planeta.application.api;
 
 import java.util.List;
+import java.util.UUID;
 import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,4 +22,8 @@ public interface PlanetaAPI {
 	@GetMapping
 	@ResponseStatus(code = HttpStatus.OK)
 	List<PlanetaListResponse> getTodosPlaneta();
+	
+	@GetMapping(value = "/{idPlaneta}")
+	@ResponseStatus(code = HttpStatus.OK)
+	PlanetaDetalhadoResponse getPlanetaAtravesId(@PathVariable UUID idPlaneta);	
 }
--- src/main/java/br/com/elo7/contraladorsondas/planeta/application/api/PlanetaController.java
+++ src/main/java/br/com/elo7/contraladorsondas/planeta/application/api/PlanetaController.java
@@ -1,9 +1,8 @@
 package br.com.elo7.contraladorsondas.planeta.application.api;
 
 import java.util.List;
-
+import java.util.UUID;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.elo7.contraladorsondas.planeta.application.service.PlanetaService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -24,9 +23,17 @@ public class PlanetaController implements PlanetaAPI {
 
 	@Override
 	public List<PlanetaListResponse> getTodosPlaneta() {
-		log.info("[inicia] ClienteController - getTodosCliente");
+		log.info("[inicia] PlanetaController - getTodosPlaneta");
 		List<PlanetaListResponse> planetas = planetaService.buscaTodosPlanetas();
-		log.info("[finaliza] ClienteController - getTodosCliente");
+		log.info("[finaliza] PlanetaController - getTodosPlaneta");
 		return planetas;
+	}
+
+	@Override
+	public PlanetaDetalhadoResponse getPlanetaAtravesId(UUID idPlaneta) {
+		log.info("[inicia] PlanetaController - getPlanetaAtravesId");
+		log.info("[idPlaneta] {}", idPlaneta);
+		log.info("[finaliza] PlanetaController - getPlanetaAtravesId");	
+		return null;
 	}
 }
