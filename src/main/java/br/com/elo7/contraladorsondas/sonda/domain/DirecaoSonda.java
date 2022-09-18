ac8c65ab622fe69f3398e3253155195e27fc19e0
Vinicius Reis <vinicius.reis@msn.com>
2022-09-18 15:41:59 -0300

    adiciona grau a direcao

--- src/main/java/br/com/elo7/contraladorsondas/sonda/domain/DirecaoSonda.java
+++ src/main/java/br/com/elo7/contraladorsondas/sonda/domain/DirecaoSonda.java
@@ -1,5 +1,14 @@
 package br.com.elo7.contraladorsondas.sonda.domain;
 
+import lombok.Getter;
+
+@Getter
 public enum DirecaoSonda {
-	NORTE, LESTE, SUL, OESTE;
+	NORTE(0), LESTE(90), SUL(180), OESTE(270);
+	
+	private Integer grau;
+
+	private DirecaoSonda(Integer grau) {
+		this.grau = grau;
+	}
 }
